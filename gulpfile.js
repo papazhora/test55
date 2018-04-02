3aa267e03cde061925785dcd849a460e3133ffc4
papazhora <4156464@gmail.com>
2018-04-02 19:06:55 +0300

    3

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,33 @@
 var gulp = require('gulp')
-    sass = require('gulp-sass');
+    concat = require('gulp-concat')
+    uglify = require('gulp-uglifyjs');
 
 
-gulp.task('sass', function(){
-    return gulp.src('app/sass/main.sass')
-        .pipe(sass())
-        .pipe(gulp.dest('app/css'))
+gulp.task('scripts', function(){
+    return gulp.src([
+        'app/libs/jquery/dist/jquery.min.js', 
+        'app/libs/magnific-popup/dist/jquery.magnific-popup.min.js'
+    ])
+    .pipe(concat('libs.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('app/js'));
+
 });
+
+
+
+
+
+
+    // sass = require('gulp-sass')
+
+
+// gulp.task('sass', function(){
+//     return gulp.src('app/sass/main.sass')
+//         .pipe(sass())
+//         .pipe(gulp.dest('app/css'))
+// });
+
+// gulp.task('watch', function(){
+//     gulp.watch('app/sass/main.sass', ['sass'])
+// });
